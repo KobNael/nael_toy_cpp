dc8e238e70ac4b0be0f0d66612e1aed6efdf5258
Aurélien Questel <aurelien.questel@planisense.com>
2025-01-16 16:40:49 +0100

    --sonar issues

--- src/lib/src/bo/BoIn.cc
+++ src/lib/src/bo/BoIn.cc
@@ -5,32 +5,26 @@ namespace bo
 
 FirstClass &BoContext::addFirstClass(std::string const &id, unsigned value)
 {
-    if(_firsts.find(id) != _firsts.end())
+    auto [iter, inserted] = _firsts.try_emplace( id, FirstClass(id, value));
+    if(!inserted)
     {
         throw std::logic_error("An item with the same id already exists");
     }
-    auto new_elem = _firsts.insert(
-        std::make_pair(
-            id, FirstClass(id, value)
-        ));
-    return new_elem.first->second;
+    return iter->second;
 }
 SecondClass &BoContext::addSecondClass(std::string const &id, std::string const &first_id, double value)
 {
-    if(_seconds.find(id) != _seconds.end())
-    {
-        throw std::logic_error("An item with the same id already exists");
-    }
     auto first = _firsts.find(first_id);
     if(first == _firsts.end())
     {
         throw std::logic_error("Unknown first item " + first_id);
     }
-    auto new_elem = _seconds.insert(
-        std::make_pair(
-            id, SecondClass(id, value, first->second)
-        ));
-    return new_elem.first->second;
+    auto [iter, inserted] = _seconds.try_emplace(id, SecondClass(id, value, first->second));
+    if(!inserted)
+    {
+        throw std::logic_error("An item with the same id already exists");
+    }
+    return iter->second;
 }
 
 }
